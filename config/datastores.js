593f0525a55b1377e007fed6f0fdea8467668ddd
senjenathaniel <topsailcashew@icloud.com>
2023-08-28 16:35:03 +0300

    Working project

--- config/datastores.js
+++ config/datastores.js
@@ -2,10 +2,9 @@ module.exports.datastores = {
   default: {
     adapter: "sails-mysql",
     host: "sql7.freemysqlhosting.net",
-    port: 3306,
+    port: "3306",
     user: "sql7642650",
     password: "aDQT3R4AS8",
     database: "sql7642650",
   },
 };
-
